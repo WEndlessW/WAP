77128ddba68aee3a65bfa1163f00a305bea1f9d0
Adam Košťál <92873657+WEndlessW@users.noreply.github.com>
2022-02-21 18:41:51 +0100

    Add files via upload

---
+++ One_Page_Web/script.js
@@ -0,0 +1,57 @@
+//První čudlík
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 600;
+}
+//Druhý čudlík
+mybutton = document.getElementById("myBtn1");
+
+window.onscroll = function() { scrollFunction() };
+
+function topFunction1() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 1400;
+}
+// Třetí čudlík
+mybutton = document.getElementById("myBtn2");
+
+window.onscroll = function() { scrollFunction() };
+
+function topFunction2() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 2050;
+}
+
+// Čtvrtý čudlík
+mybutton = document.getElementById("myBtn3");
+
+window.onscroll = function() { scrollFunction() };
+
+function topFunction3() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 2800;
+}
+
+// Patý čudlík
+mybutton = document.getElementById("myBtn4");
+
+window.onscroll = function() { scrollFunction() };
+
+function topFunction4() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 3400;
+}
+
+//Šestý čudlík na rychlý scroll nahoru
+mybutton = document.getElementById("myBtn5");
+
+window.onscroll = function() { scrollFunction() };
+
+function topFunction5() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
