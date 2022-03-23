12d0fafd7f1a729283d4444454eb70823a74a1a1
Adam Košťál <92873657+WEndlessW@users.noreply.github.com>
2022-03-23 19:05:00 +0100

    Add files via upload

---
+++ aukcni_web/data.js
@@ -0,0 +1,32 @@
+const data = {
+
+    art1: {
+        name: "Super Mario",
+        description: "Skákající Super Mario",
+        author: "IDK",
+        price: "2 000Kč",
+        images: ["Super_mario.jpg"]
+
+    },
+    art2: {
+        name: "Sussy Baka",
+        description: "Je to sus",
+        author: "Amogus",
+        price: "420 69Kč",
+        images: ["sus.jpg"]
+    },
+    art3: {
+        name: "Anime",
+        description: "Prostě anime holka",
+        price: "20000 Yens",
+        author: "Japonsko",
+        images: ["anime_gurl.jpg"]
+    },
+    art4: {
+        name: "League of Degens",
+        description: "Je to Dobrá hra OMEGALUL",
+        price: "0Kč",
+        author: "Riot",
+        images: ["ahri.jpg"]
+    },
+};
---
+++ aukcni_web/script.js
@@ -0,0 +1,18 @@
+const gallery = document.getElementById("gallery");
+const detail = document.getElementById("detail");
+
+for (let index in data) {
+    console.log(index);
+    let item = data[index];
+    let p = document.createElement("div");
+    p.innerHTML = `<img src="${item.images[0]}"><h2 id="name">${item.name}</h2><p> ${item.description}</p><p> ${item.author}</p><p class="price">${item.price}</p>`;
+    gallery.appendChild(p);
+
+    p.onclick = () => {
+        detail.innerHTML = `<div class="background"><div class="deta"><img src="${item.images[0]}"  class="detailPic"></div></div>`;
+        detail.style.display = "flex";
+    }
+    detail.onclick = () => {
+        detail.style.display = "none"
+    }
+}
